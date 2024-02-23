de236cd4b55fc5411bfbabfedd2f11a29cb75f35
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2024-02-23 10:35:06 +0100

    lektion 10 - javascript(fetch) och web api

---
+++ lektion-10/javascript/WebApi/Repositories/Repo.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebApi.Contexts;
+
+namespace WebApi.Repositories;
+
+public abstract class Repo<TEntity>(DataContext context) where TEntity : class
+{
+    private readonly DataContext _context = context;
+
+    public virtual async Task<TEntity> CreateAsync(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+        catch { }
+        return null!;
+    }
+
+    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            return result!;
+        }
+        catch { }
+        return null!;
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAsync()
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().ToListAsync();
+            return result!;
+        }
+        catch { }
+        return null!;
+    }
+
+    public virtual async Task<TEntity> UpdateAsync(Expression<Func<TEntity, bool>> predicate, TEntity entity)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null) 
+            { 
+                _context.Entry(result).CurrentValues.SetValues(entity);
+                await _context.SaveChangesAsync();
+            }
+            return result!;
+        }
+        catch { }
+        return null!;
+    }
+
+    public virtual async Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null)
+            {
+                _context.Set<TEntity>().Remove(result);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+        catch { }
+        return false;
+    }
+}
--- lektion-10/javascript/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ lektion-10/javascript/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc0ff2442d44c5e2f68281d23e560c580e0d198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706fb53d7f8fb8230c36fc5c5dc5046a4650b605")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
