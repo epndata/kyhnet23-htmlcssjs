de1e0a91c9775f018496f0692e31f1413dc2e230
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2024-03-01 09:00:10 +0100

    lektion 12 - react hooks - Routing added

--- lektion-12/reacthooks/src/main.jsx
+++ lektion-12/reacthooks/src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './assets/css/main.min.css'
+import Home from './views/Home'
+import Error from './views/Error'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
 
   </React.StrictMode>,
 )
